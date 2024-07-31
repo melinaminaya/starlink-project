6f7d09ce77c8a318f763ff9d5ebb63d8a27928ae
Melina Minaya <melina.minaya@autotrac.com.br>
2024-07-31 10:58:07 -0300

    Include everything on develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -88,3 +88,86 @@ import { DropdownUfComponent } from "./shared/form-busca/dropdown-uf/dropdown-uf
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { CardComponent } from './shared/card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { ContainerComponent } from './shared/container/container.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
+import { MatCardModule } from '@angular/material/card';
+import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
+import { FormBuscaComponent } from './shared/form-busca/form-busca.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ModalComponent } from './shared/modal/modal.component';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CardDashboardComponent } from './shared/card-dashboard/card-dashboard.component';
+import {MatSelectModule} from '@angular/material/select';
+import { PromocoesComponent } from './pages/home/promocoes/promocoes.component';
+import { LeafletMapComponent } from './shared/leaflet-map/leaflet-map.component';
+import { InformationContainerComponent } from './shared/information-container/information-container.component';
+import { NgxChartsModule } from "@swimlane/ngx-charts";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    CardComponent,
+    ContainerComponent,
+    HomeComponent,
+    CardBuscaComponent,
+    CardDepoimentoComponent,
+    FormBuscaComponent,
+    ModalComponent,
+    SidenavComponent,
+    CardDashboardComponent,
+    PromocoesComponent,
+    LeafletMapComponent,
+    InformationContainerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    MatSidenavModule,
+    MatListModule,
+    HttpClientModule,
+    MatSelectModule,
+    NgxChartsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
